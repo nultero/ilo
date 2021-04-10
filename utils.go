b067c54fb133d12e800216dc740921976d045927
nultero <59940962+nultero@users.noreply.github.com>
2021-04-10 15:25:55 -0500

    cleaner list function w/out individual array pieces

--- utils.go
+++ utils.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 // outline --
@@ -23,7 +24,7 @@ import (
 // onto files, so this private func makes the end filenames
 // slightly more legible / transparent for finding them in
 // file exploration. (and also makes file grab easier)
-func txtCaser(location string) string {
+func txtFileNamer(location string) string {
 	switch location {
 
 	case "onetime":
@@ -51,10 +52,42 @@ func txtCaser(location string) string {
 //  |     |  |||   |||
 
 // takes & adds a str input onto a given filetype
-func Add(typeOf string) {
-	typeOf = txtCaser(typeOf)
+func Add(typeOf string, promptstr string) {
+	typeOf = txtFileNamer(typeOf)
 	readPath := ArgCleaner("homedir") + typeOf
 	conts, _ := os.ReadFile(readPath)
 	typeOf = string(conts)
-	fmt.Println(typeOf) // read to byte, restring and all still left
+	fmt.Println("Thing to add:")
+	dat := HandleOptionsInput(promptstr)
+	typeOf = typeOf + "\n" + dat
+	// still needs stuff like for DateTimes and whatnot
+	//
+	//
+	os.WriteFile(readPath, []byte(typeOf), 0644)
+}
+
+//  ||   ||| |||| |||
+//  ||    |   |    |
+//  ||    |    |   |
+//  |||| ||| ||||  |
+
+// Lists contents of argpath. What it says on the tin.
+func List(typeOf string) {
+	typeOf = txtFileNamer(typeOf)
+	fmt.Println(typeOf + " > test")
+	readPath := ArgCleaner("homedir") + typeOf
+	fmt.Println(readPath + " > test")
+	conts, _ := os.ReadFile(readPath)
+	fmt.Println(string(conts))
+}
+
+func Test() {
+	TermClear()
+	fmt.Print("Input? : ")
+	// i := HandleOptionsInput("> ")
+	fmt.Println("red", "", strings.Repeat("Jackson", 90))
+}
+
+func TermClear() {
+	fmt.Print("\033[H\033[2J")
 }
