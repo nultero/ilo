5f01f54b4235b2916eb32c81aa388bc875faefae
nultero <nultero@protonmail.com>
2021-09-09 17:57:19 -0500

    nope, not doing rawterm

--- src/bx/timeline.rs
+++
@@ -1,8 +0,0 @@
-const MONTHS: [&'static str; 12] = [
-    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
-];
-
-pub fn get_month(i: u32) -> String {
-    let i = i as usize;
-    return MONTHS[i].to_owned();
-}
